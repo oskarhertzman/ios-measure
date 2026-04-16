cbd58f58978bee0d35deedc9a184bddd13310304
Oskar Hertzman <oskar.hertzman@ecsolutions.se>
2026-04-16 10:09:13 +0200

    Initial Commit

---
+++ MeasurementTest/MeasurementTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeasurementTestApp.swift
+//  MeasurementTest
+//
+//  Created by Oskar Hertzman on 2026-04-16.
+//
+
+import SwiftUI
+
+@main
+struct MeasurementTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
